c5114cc5b0e9a79f4e3a339cd4234be0bda6cb52
anna <a.chervonuk@platim.ru>
2023-06-13 18:40:31 +0300

    add

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,65 +1,81 @@
 <template>
+  <div>{{ selectedHTML }}</div>
   {{ currentMode }}
   <select v-model="currentMode">
-    <option v-for="mode in listOfModes" :key="mode">
+    <option v-for="mode in listOfModes" :key="mode" :value="mode">
       {{ mode }}
     </option>
   </select>
-  <div v-html="content"></div>
+  <div :style="stylesForContent">
+    <div class="content" v-html="content"></div>
+  </div>
+  <component :is="'style'">
+    {{ stylesForContent }}
+  </component>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref } from "vue";
+<script setup lang="ts">
+import { ref, computed } from "vue";
 
 const HTMLcontent = `
-<div>
-This prevents starting the selection on elem, but the visitor may start it at another element, then extend to elem.
-That’s convenient when there’s another event handler on the same action that triggers the select (e.g. mousedown). So we disable the selection to avoid conflict, still allowing elem contents to be copied.
-We can also clear the selection post-factum after it happens wih document.getSelection().empty(). That’s rarely used, as this causes unwanted blinking as the selection appears-disappears.
-</div>
-<div>
-1
-</div>
-<div>
-This prevents starting the selection on elem, but the visitor may start it at another element, then extend to elem.
-That’s convenient when there’s another event handler on the same action that triggers the select (e.g. mousedown). So we disable the selection to avoid conflict, still allowing elem contents to be copied.
-We can also clear the selection post-factum after it happens wih document.getSelection().empty(). That’s rarely used, as this causes unwanted blinking as the selection appears-disappears.
-</div>
+    <div>
+      <span class="all">1This is an about page </span
+      ><span class="main">main1This</span> is an about page1This<span
+        class="interesting"
+      >
+        interestingis</span
+      >
+      an about page 1This is an about page 1This is an about page
+    </div>
+    <div>
+      <span class="all">2This </span><span class="main">mainis an about</span
+      ><span class="all"> page</span>
+    </div>
+    <div><span class="all">3This is an about page</span></div>
 `;
-export default defineComponent({
-  name: "HelloWorld",
-  setup() {
-    const content = ref<string>(HTMLcontent);
-    const currentMode = ref<string>("all");
-    const listOfModes = ref<string[]>([
-      "main",
-      "interesting",
-      "off-topic",
-      "all",
-    ]);
-    return {
-      content,
-      currentMode,
-      listOfModes,
-    };
-  },
+const content = ref<string>(HTMLcontent);
+const currentMode = ref<string>("all");
+const listOfModes = ref<string[]>(["main", "interesting", "off-topic", "all"]); // должны быть уникальные значения
+const selectedHTML = ref<string>("");
+
+document.onselectionchange = () => {
+  console.log("document.onselectionchange");
+
+  let selection = document.getSelection();
+
+  if (!selection) {
+    return;
+  }
+
+  let { anchorNode, anchorOffset, focusNode, focusOffset } = selection;
+
+  console.log(`${anchorNode}, offset ${anchorOffset}`);
+  console.log(`${focusNode}, offset ${focusOffset}`);
+
+  selectedHTML.value = "";
+
+  // Clone DOM nodes from ranges (we support multiselect here)
+  for (let i = 0; i < selection.rangeCount; i++) {
+    const fragment = selection.getRangeAt(i).cloneContents();
+    var div = document.createElement("div");
+    div.appendChild(fragment);
+    console.log(div.innerHTML);
+    selectedHTML.value += div.innerHTML;
+  }
+
+  // check selection in one Node
+  console.log(anchorNode === focusNode);
+};
+
+const stylesForContent = computed(() => {
+  const index = listOfModes.value.indexOf(currentMode.value);
+  if (index === -1) {
+    return "";
+  } else {
+    return listOfModes.value
+      .slice(index + 1)
+      .map((mode) => `.${mode} {display: none;} `)
+      .join("\n");
+  }
 });
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="content">
+    <div>
+      <span class="all">1This is an about page </span
+      ><span class="main">main1This</span> is an about page1This<span
+        class="interesting"
+      >
+        interestingis</span
+      >
+      an about page 1This is an about page 1This is an about page
+    </div>
+    <div>
+      <span class="all">2This </span><span class="main">mainis an about</span
+      ><span class="all"> page</span>
+    </div>
+    <div><span class="all">3This is an about page</span></div>
   </div>
 </template>
+
+<script setup lang="ts"></script>
+
+<style>
+.all {
+  display: none;
+}
+</style>
