0faa2cfb91d490109e9e191f861289c432bc4c16
anna <a.chervonuk@platim.ru>
2023-06-13 11:51:25 +0300

    add

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,114 +1,47 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-  </div>
+  {{ currentMode }}
+  <select v-model="currentMode">
+    <option v-for="mode in listOfModes" :key="mode">
+      {{ mode }}
+    </option>
+  </select>
+  <div v-html="content"></div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
+const HTMLcontent = `
+<div>
+This prevents starting the selection on elem, but the visitor may start it at another element, then extend to elem.
+That’s convenient when there’s another event handler on the same action that triggers the select (e.g. mousedown). So we disable the selection to avoid conflict, still allowing elem contents to be copied.
+We can also clear the selection post-factum after it happens wih document.getSelection().empty(). That’s rarely used, as this causes unwanted blinking as the selection appears-disappears.
+</div>
+<div>
+1
+</div>
+<div>
+This prevents starting the selection on elem, but the visitor may start it at another element, then extend to elem.
+That’s convenient when there’s another event handler on the same action that triggers the select (e.g. mousedown). So we disable the selection to avoid conflict, still allowing elem contents to be copied.
+We can also clear the selection post-factum after it happens wih document.getSelection().empty(). That’s rarely used, as this causes unwanted blinking as the selection appears-disappears.
+</div>
+`;
 export default defineComponent({
   name: "HelloWorld",
-  props: {
-    msg: String,
+  setup() {
+    const content = ref<string>(HTMLcontent);
+    const currentMode = ref<string>("all");
+    const listOfModes = ref<string[]>([
+      "main",
+      "interesting",
+      "off-topic",
+      "all",
+    ]);
+    return {
+      content,
+      currentMode,
+      listOfModes,
+    };
   },
 });
 </script>
